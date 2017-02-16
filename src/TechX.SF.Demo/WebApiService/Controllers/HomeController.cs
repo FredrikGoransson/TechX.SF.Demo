cd06118944e9f63f72767f593d25ab0e7fff8532
Fredrik Göransson <fredrik.goransson@gmail.com>
2017-02-16 22:09:38 +0100

    WebApi call actor by actor name

--- src/TechX.SF.Demo/WebApiService/Controllers/HomeController.cs
+++ src/TechX.SF.Demo/WebApiService/Controllers/HomeController.cs
@@ -1,15 +1,43 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Http;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using SpeakerActor.Interfaces;
 
 namespace WebApiService.Controllers
 {
 	[ServiceRequestActionFilter]
 	public class HomeController : ApiController
 	{
-		// GET api/values 
-		public IEnumerable<string> Get()
+		private readonly IActorProxyFactory _actorProxyFactory;
+
+		public HomeController()
 		{
-			return new string[] { "value1", "value2" };
+			_actorProxyFactory = new ActorProxyFactory();
+		}
+
+		// GET /home/Fredrik%20G%C3%B6ransson
+		public async Task<SpeakerDetails> Get(string id)
+		{
+			var cancelationToken = CancellationToken.None;
+
+			var speakerActor = _actorProxyFactory.CreateActorProxy<ISpeakerActor>(new ActorId(id));
+			var speakerInfo = await speakerActor.GetSpeakerInfoAsync(cancelationToken);
+			var sessionsInfo = await speakerActor.GetSessionsAsync(cancelationToken);
+			var speaker = new SpeakerDetails()
+			{
+				Name = speakerInfo.Name,
+				Bio = speakerInfo.Bio,
+				Sessions = sessionsInfo,
+			};
+
+			return speaker;
 		}
 	}
 }
